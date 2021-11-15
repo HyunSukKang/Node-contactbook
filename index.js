666d08b57309a66431f1ebde0e25680d23ab61a0
HyunSukKang <hsuk6032@gmail.com>
2021-11-15 21:35:20 +0900

    export module

--- index.js
+++ index.js
@@ -23,72 +23,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
-// DB schema
-var contactSchema = mongoose.Schema({
-  name:{type:String, required:true, unique:true},
-  email:{type:String},
-  phone:{type:String},
-});
-var Contact = mongoose.model('contact', contactSchema);
-
 // Routes
-// Home
-app.get('/', function(req, res) {
-  res.redirect('/contacts');
-});
+app.use('/', require('./routes/home'));
+app.use('/contacts', require('./routes/contacts'));
 
-// Contacts - Index
-app.get('/contacts', function(req, res) {
-  Contact.find({}, function(err, contacts) {
-    if(err) return res.json(err);
-    res.render('contacts/index', {contacts:contacts});
-  });
-});
-
-// Contacts - New
-app.get('/contacts/new', function(req, res) {
-  res.render('contacts/new');
-});
-
-// Contacts - Create
-app.post('/contacts', function(req, res) {
-  Contact.create(req.body, function(err, contact) {
-    if(err) return res.json(err);
-    res.redirect('contacts');
-  });
-});
-
-// Contacts - Show
-app.get('/contacts/:id', function(req, res) {
-  Contact.findOne({_id:req.params.id}, function(err, contact) {
-    if(err) return res.json(err);
-    res.render('contacts/show', {contact:contact});
-  });
-});
-
-// Contacts - Edit
-app.get('/contacts/:id/edit', function(req, res) {
-  Contact.findOne({_id:req.params.id}, function(err, contact) {
-    if(err) return res.json(err);
-    res.render('contacts/edit', {contact:contact});
-  });
-});
-
-// Contacts - Update
-app.put('/contacts/:id', function(req, res) {
-  Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact) {
-    if(err) return res.json(err);
-    res.redirect('/contacts/' + req.params.id);
-  });
-});
-
-// Contacts - Destory
-app.delete('/contacts/:id', function(req, res) {
-  Contact.deleteOne({_id:req.params.id}, function(err) {
-    if(err) return res.json(err);
-    res.redirect('/contacts');
-  });
-});
 
 // Port settings
 var port = 3000;
